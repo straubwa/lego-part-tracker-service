46a23e254d16393b638ac0e35988764da6904964
Wayne Straub <waynestraub@gmail.com>
2018-12-28 16:47:05 -0500

    cleanup

--- LegoPartTracker.API/Models/SetPartDto.cs
+++ LegoPartTracker.API/Models/SetPartDto.cs
@@ -32,24 +32,3 @@ namespace LegoPartTracker.API.Models
     }
 }
 
-  // code from rebrickable api
-
-  //"count": 140,
-  //"next": "https://rebrickable.com/api/v3/lego/sets/70611-1/parts/?page=2",
-  //"previous": null,
-  //"results": [
-  //  {
-  //    "id": 1244791,
-  //    "inv_part_id": 1244791,
-  //    "part": {
-  //      "part_num": "63965",
-  //      "name": "Bar 6L with Stop Ring",
-  //      "part_url": "https://rebrickable.com/parts/63965/bar-6l-with-stop-ring/",
-  //      "part_img_url": "https://m.rebrickable.com/media/parts/elements/4533907.jpg",
-  //    },
-  //    "color": {
-  //      "name": "Black"
-  //    },
-  //    "quantity": 3,
-  //    "element_id": "6081988"
-  //  }
